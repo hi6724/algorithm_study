00abcfa81b065cbe694042102448e4b53ae90fde
hi6724 <hi6724@gmail.com>
2022-05-11 11:30:59 +0900

    코멘트 추가

--- 5.9_greedy/jewelry.py
+++ 5.9_greedy/jewelry.py
@@ -9,15 +9,18 @@ bags = []
 for _ in range(K):
     bags.append(int(sys.stdin.readline()))
 bags.sort()
+print(bags)
 answer = 0
 tmp_jew = []
+# bag 가벼운 순으로 탐색
+# 첫번째 가방에 들어간다 => 다음가방에는 당연히 들어갈 수 있음
+# 보석의 가치가 높은 순으로 하나 씩 꺼 내면 답
 for bag in bags:
-    print(tmp_jew)
-
     while jew and bag >= jew[0][0]:
         heapq.heappush(tmp_jew, -heapq.heappop(jew)[1])
     if tmp_jew:
         answer -= heapq.heappop(tmp_jew)
     elif not jew:
         break
+
 print(answer)
